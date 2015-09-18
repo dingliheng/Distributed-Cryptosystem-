0f0e937e677dad2fe34bb109a99e2ce5ec29559c
Liheng DIng <dinglihengusapp@163.com>
2015-09-18 18:39:50 -0500

    add a "seat" class

--- assignment1/src/com/distsys/jun/seat.java
+++ assignment1/src/com/distsys/jun/seat.java
@@ -4,7 +4,7 @@ import java.util.*;
  * Created by Liheng on 2015/9/18.
  */
 public class seat {
-    public static Map num_name = new HashMap();
+    public Map<Integer, String> num_name = new HashMap<Integer, String>();
     public static int c;
     public int seats_reserved;
     public String message_to_client;
@@ -26,35 +26,31 @@ public class seat {
         if (count > (c - seats_reserved)) {
             message_to_client = String.format("Failed:only %d seats left but %d seats are requested", c - seats_reserved, count);
             return message_to_client;
-        }else{
-            name_seats_reserved = 0;
-            seat_number.clear();
-            for (int i = 1; i <= c; i++) {
-                if (num_name.get(i) == name) {
+        }
+        name_seats_reserved = 0;
+        seat_number.clear();
+        for (int i = 1; i <= c; i++) {
+            if (num_name.get(i) == name) {
+                name_seats_reserved++;
+                seat_number.add(i);
+            }
+        }
+        if (name_seats_reserved!=0) {
+            message_to_client = String.format("Failed: "+name+" has booked the following seats:"+seat_number+".");
+            return message_to_client;
+        }
+        for (int i = 1; i <= c; i++) {
+            if(name_seats_reserved<count) {
+                if (num_name.get(i) == "") {
                     name_seats_reserved++;
                     seat_number.add(i);
                 }
-            }
-            if (name_seats_reserved!=0) {
-                message_to_client = String.format("Failed: "+name+" has booked the following seats:"+seat_number+".");
-                return message_to_client;
             }else{
-                //name_count_reserved = 0;
-                //seat_number.clear();
-                for (int i = 1; i <= c; i++) {
-                    if(name_seats_reserved<count) {
-                        if (num_name.get(i) == "") {
-                            name_seats_reserved++;
-                            seat_number.add(i);
-                        }
-                    }else{
-                        break;
-                    }
-                }
-                message_to_client = String.format("The seats have been reversed for "+name+":"+seat_number+".");
-                return message_to_client;
+                break;
             }
         }
+        message_to_client = String.format("The seats have been reversed for "+name+":"+seat_number+".");
+        return message_to_client;
     }
     public static void main(String[] args) {
         seat a = new seat(10);
