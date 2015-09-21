dd815005665701269c1f1816a2a9fa8e638be99e
Liheng DIng <dinglihengusapp@163.com>
2015-09-21 11:57:25 -0500

    add fault tolerance to Client

--- assignment1/src/com/distsys/jun/Client.java
+++ assignment1/src/com/distsys/jun/Client.java
@@ -49,6 +49,28 @@ public class Client {
                             try{
                                 Socket skt = new Socket("localhost", portList.get(random_port));
                                 System.out.println("connect successfully");
+                                InputStream in = skt.getInputStream();
+                                OutputStream out = skt.getOutputStream();
+                                //                PrintWriter os = new PrintWriter(skt.getOutputStream());
+                                MessageClosure clientmessage = new MessageClosure(line);
+                                socketObjSend(clientmessage, out);
+                                //                os.println(line);
+                                //                os.flush();
+                                ////                System.out.print("Received string:'");
+                                //                while (!in.ready()) {}
+                                //                System.out.println(in.readLine()); // Read one line and output it
+                                MessageClosure receivedmessage = (MessageClosure) socketObjReceive(in);
+                                if (receivedmessage.getMyType() != String.class){
+                                    System.out.println("Wrong message");
+                                }
+                                System.out.print(receivedmessage.toString());
+                                System.out.print("\n");
+                                out.flush();
+                                in.close();
+                                out.close();
+                                skt.close();
+                                System.out.println("Please import your request");
+                                line=sin.readLine();
                                 connected = true;
                                 break;
                             }catch (Exception e){
@@ -58,32 +80,6 @@ public class Client {
                             }
                         }
 //                    Socket skt = new Socket("localhost", 1234); //random port}
-                        if (connected = true){
-                            Socket skt = new Socket("localhost", portList.get(random_port));
-
-                            InputStream in = skt.getInputStream();
-                            OutputStream out = skt.getOutputStream();
-                            //                PrintWriter os = new PrintWriter(skt.getOutputStream());
-                            MessageClosure clientmessage = new MessageClosure(line);
-                            socketObjSend(clientmessage, out);
-                            //                os.println(line);
-                            //                os.flush();
-                            ////                System.out.print("Received string:'");
-                            //                while (!in.ready()) {}
-                            //                System.out.println(in.readLine()); // Read one line and output it
-                            MessageClosure receivedmessage = (MessageClosure) socketObjReceive(in);
-                            if (receivedmessage.getMyType() != String.class){
-                                System.out.println("Wrong message");
-                            }
-                            System.out.print(receivedmessage.toString());
-                            System.out.print("\n");
-                            out.flush();
-                            in.close();
-                            out.close();
-                            skt.close();
-                            System.out.println("Please import your request");
-                            line=sin.readLine();
-                        }
                     }
                 } catch(Exception e) {
                     System.out.print(e.getClass().getName() + "\n" + e.getMessage() + "\n");
